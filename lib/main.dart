ca4be59e426cbdd78263163ab6456ace4278f415
Prince Enacton <pdhirapara@enacton.com>
2024-08-23 17:30:05 +0530

    feat: init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
